03cbcd9b171e9236bbcf08ab12cb09ea714e0e00
José Eduardo Pedralino da Silva <52474533+Edu-Jeps@users.noreply.github.com>
2025-10-27 18:31:41 +0000

    criação da rota `POST/tasks`

---
+++ src/routes.js
@@ -0,0 +1,28 @@
+import { Database } from "./database.js";
+import { buildRoutePath } from "./utils/build-route-path.js";
+import { randomUUID } from 'node:crypto';
+
+const database = new Database()
+
+export const routes = [
+    {
+        method: 'POST',
+        path: buildRoutePath('/tasks'),
+        handler: (req, res) => {
+            const { title, description } = req.body
+
+            const task = {
+                id: randomUUID(),
+                title, 
+                description, 
+                completed_at: null,
+                created_at: new Date().toISOString(), 
+                updated_at: new Date().toISOString(),
+            }
+
+            console.log(task)
+            database.insert('tasks', task)
+            return res.writeHead(201).end()
+        }
+    }
+]
